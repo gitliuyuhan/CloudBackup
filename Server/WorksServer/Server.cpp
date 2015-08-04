f7e1d4e92b7ae54ac2ba94660680d6c1433765da
wangweihao <578867817@qq.com>
2015-08-04 23:53:15 +0800

    update

--- Server/WorksServer/Server.cpp
+++ Server/WorksServer/Server.cpp
@@ -19,13 +19,12 @@
 #include <Server.h>
 #include <sys/sendfile.h>
 #include <sys/stat.h>
+#include <json/json.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-bool func(int fd, std::string json)
-{
-    std::cout << "文件描述符为:" << fd << " Json:" << json << std::endl;
-    return true;
-}
 
+/* constructor */
 workServer::
 workServer(std::string m_ip, int m_port):
     ip(m_ip), port(m_port), threadpool(10)
@@ -35,6 +34,7 @@ workServer(std::string m_ip, int m_port):
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
     inet_pton(AF_INET, ip.c_str(), &server.sin_addr);
+
     /*创建普通socket */
     listen_fd = socket(AF_INET, SOCK_STREAM, 0);
     if(listen_fd < 0)
@@ -42,6 +42,43 @@ workServer(std::string m_ip, int m_port):
         perror("create socket error\n");
         exit(1);
     }
+
+    /* set recv buffer size */
+    int recvSize = 200000;
+    int recvLen = sizeof(recvSize);
+    int recv_sret = setsockopt(listen_fd, SOL_SOCKET, SO_RCVBUF, &recvSize, sizeof(recvSize));
+    if(recv_sret < 0)
+    {
+        perror("recv buffer setsockopt error\n");
+        exit(1);
+    }
+    /* get recv buffer size */
+    int recv_gret = getsockopt(listen_fd, SOL_SOCKET, SO_RCVBUF, &recvSize, (socklen_t*)&recvLen);
+    if(recv_gret < 0)
+    {
+        perror("recv buffer getsockopt error\n");
+        exit(1);
+    }
+    std::cout << "size:" << recvSize << std::endl;
+
+    /* set send buffer size */
+    int sendSize = 200000;
+    int sendLen = sizeof(sendSize);
+    int send_sret = setsockopt(listen_fd, SOL_SOCKET, SO_SNDBUF, &sendSize, sizeof(sendSize));
+    if(send_sret < 0)
+    {
+        perror("send buffer setsockopt error\n");
+        exit(1);
+    }
+    /* get send buffer size */
+    int send_gret = getsockopt(listen_fd, SOL_SOCKET, SO_SNDBUF, &sendSize, (socklen_t*)&sendLen);
+    if(send_gret < 0)
+    {
+        perror("send buffer getsockopt error\n");
+        exit(1);
+    }
+    std::cout << "size:" << sendSize << std::endl;
+
     /* 绑定端口 */
     int ret = bind(listen_fd, (struct sockaddr*)&server, sizeof(server));
     if(ret == -1)
@@ -49,6 +86,7 @@ workServer(std::string m_ip, int m_port):
         perror("bind socket error\n");
         exit(1);
     }
+
     /* 转换为监听socket，将backlog设置为1024 */
     ret = listen(listen_fd, 1024);
     if(ret == -1)
@@ -56,9 +94,13 @@ workServer(std::string m_ip, int m_port):
         perror("listen error\n");
         exit(1);
     }
+
+    /* create epoll fd and Register fd epoll collection */
     epoll_fd = epoll_create(maxCon);
     Register(listen_fd, false);
 }
+
+
 
 /* destroy function  close socket fd*/
 workServer::
@@ -67,6 +109,7 @@ workServer::
     close(listen_fd);
     close(epoll_fd);
 }
+
 
 
 /* set socket fd nonblocking */
@@ -79,6 +122,7 @@ setnonblocking(int fd)
     int ret = fcntl(fd, F_SETFL, new_option);
     return old_option;
 }
+
 
 
 /* register sockfd in epoll */
@@ -98,11 +142,22 @@ Register(int fd, bool oneshot)
     return 0;
 }
 
+
+
 /* epoll run */
 int 
 workServer::
 Run()
 {
+    /* json parse */
+    Json::Reader reader;
+    Json::Value value;
+
+    /* mark and md5 */
+    int mark = 0;
+    std::string md5;
+
+    /* main loop */
     while(1)
     {
         int ret = epoll_wait(epoll_fd, events, maxCon, -1);
@@ -124,25 +179,41 @@ Run()
             }
             else if(events[i].events & EPOLLIN)
             {
-                //std::cout << "读事件产生" << std::endl;
                 //std::cout << "添加到任务队列中,等待线程处理" << std::endl;
+                
                 /* receive handler pass on threadpool */ 
-                //Handler hand = std::make_tuple(&workServer::handler_upload, 10, "hello world");
                 char buffer[1024];
                 recv(sockfd, buffer, 1024, 0);
-                std::string s(buffer);
-                printf("%s\n", buffer);
-                /* download */
-                //Handler hand = std::make_tuple(workServer::handler_download, sockfd, s);
-                //threadpool.AddTask(std::move(hand));
-                /* upload */
-                Handler hand = std::make_tuple(workServer::handler_upload, sockfd, s);
-                threadpool.AddTask(std::move(hand));
+                std::string jjson(buffer);
+                std::cout << jjson << std::endl;
+                if(reader.parse(jjson, value))
+                {
+                    if(!value["mark"].isNull())
+                    {
+                        mark = value["mark"].asInt();
+                        md5 = value["md5"].asString();
+                        std::cout << mark << " " << md5 << std::endl; 
+                    }
+                }
 
+                /* judge mark */
+                if(mark == 1)
+                {
+                    /* download */
+                    Handler hand = std::make_tuple(workServer::handler_download, sockfd, md5);
+                    threadpool.AddTask(std::move(hand));
+                }else if(mark == 2)
+                {
+                    /* upload */
+                    Handler hand = std::make_tuple(workServer::handler_upload, sockfd, md5);
+                    threadpool.AddTask(std::move(hand));
+                }
             }
         }
     }
 }
+
+
 
 /* download file */
 bool
@@ -200,6 +271,7 @@ handler_download(int fd, std::string json)
 }
 
 
+
 /* upload file */
 bool
 workServer::
@@ -216,24 +288,40 @@ handler_upload(int fd, std::string json)
         exit(1);
     }
     char buffer[20480];
-    while(1)
+    //while(1)
+    //{
+    //    bzero(buffer, 20480);
+    //    int n = recv(fd, buffer, 20480, 0);
+    //    if(n < 0)
+    //    {
+    //        perror("recv error\n");
+    //        exit(1);
+    //    }else if(n == 0)
+    //    {
+    //        break;
+    //    }
+    //    ssize_t s_t = write(w_fd, buffer, n);
+    //    if(s_t < 0)
+    //    {
+    //        perror("write error\n");
+    //        exit(1);
+    //    }
+    //}
+    int pipefd[2];
+    int ret = pipe(pipefd);
+    ret = splice(fd, NULL, pipefd[1], NULL, 32768,
+            SPLICE_F_MORE | SPLICE_F_MOVE);
+    if(ret == -1)
     {
-        bzero(buffer, 20480);
-        int n = recv(fd, buffer, 20480, 0);
-        if(n < 0)
-        {
-            perror("recv error\n");
-            exit(1);
-        }else if(n == 0)
-        {
-            break;
-        }
-        ssize_t s_t = write(w_fd, buffer, n);
-        if(s_t < 0)
-        {
-            perror("write error\n");
-            exit(1);
-        }
+        perror("splice error\n");
+        exit(1);
+    }
+    ret = splice(pipefd[0], NULL, w_fd, NULL, 32768,
+            SPLICE_F_MORE | SPLICE_F_MOVE);
+    if(ret == -1)
+    {
+        perror("splice error\n");
+        exit(1);
     }
 }
 
