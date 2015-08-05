89825d2c5297cb99b210a2576912136fb5a92956
wangweihao <578867817@qq.com>
2015-08-05 15:30:20 +0800

    try mode

--- Server/WorksServer/main.cpp
+++ Server/WorksServer/main.cpp
@@ -25,7 +25,9 @@ int main(int argc, char *argv[])
 {
     std::string ip(argv[1]);
     int port = atoi(argv[2]);
-	workServer server(ip, port);
+    std::string l_ip(argv[3]);
+    int l_port = atoi(argv[4]);
+	workServer server(ip, port, l_ip, l_port);
     server.Run();
 
 	return EXIT_SUCCESS;
