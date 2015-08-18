7c4e5f858392179d5c8be464d65b2f57da241b42
wangweihao <578867817@qq.com>
2015-08-18 21:31:59 +0800

    update

--- Server/BalanceServer/balanceServer.py
+++ Server/BalanceServer/balanceServer.py
@@ -3,7 +3,11 @@
 
 import socket
 import select
-import Queue
+import time
+import threading
+import re
+
+
 
 #创建服务端socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,19 +16,89 @@ server.setblocking(False)
 #设置可重用端口
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 #设置ip和端口并绑定
-server_address = ('192.168.20.150', 12345)
+server_address = ('192.168.20.184', 12345)
 server.bind(server_address)
 #监听并设置监听队列大小
 server.listen(10)
 
+#事件集合
 inputs = [server]
 outputs = []
-message_queues = {}
 
+#子服务器connection
+workServer_fd = []
+
+#select 超时时间
 timeout = 20
 
 result = []
 
+#创建定时发送信号线程
+def threadFunc():
+    while 1:
+        #定时为4秒
+        time.sleep(4)
+        #向集合中每个workerServer发送信号
+        print "send signal"
+        for fd in workServer_fd:
+            fd.send("send signal")
+
+#创建线程，执行定时发送信号任务
+tid = threading.Thread(target = threadFunc)
+tid.start()
+
+
+#提取数据
+def parse(s):
+    #按照split分割
+    t = s.split('split')
+    #print t[0]
+    #print t[1]
+    #print t[2]
+    #print t[3]
+    #获得cpu使用率
+    cre = re.search('ni,(.+?)id', t[0])
+    #cre = re.search('id', t[0])
+    s1 = cre.group(1)
+    ss1 = s1.split(' ')
+    #print ss1[1]
+    i0 = float(ss1[1])
+    #print i0
+    #print type(i0)
+    #获得内存空闲大小
+    mre = re.search('used,(.+?)free,', t[1])
+    #print mre.group(1)
+    s2 = mre.group(1)
+    ss2 = s2.split(' ')
+    #print ss2[3]
+    i2 = int(ss2[3])
+    #print i2
+    #print type(i2)
+    #i1 = int(mre.group(1))
+    #获得带宽占用 rxkB/s txkB/s
+    ss3 = t[2].split('     ')
+    tp = []
+    for i3 in range(len(ss3)):
+        if ss3[i3] not in ' ':
+           tp.append(ss3[i3]) 
+    #    print i3,ss3[i3]
+    #print 'xxxxxxxxxxxxxxx'
+    #for ttt in tp:
+    #    print ttt
+    rx = float(tp[4])
+    tx = float(tp[5])
+    #print rx, tx
+    #print 'xxxxxxxxxxxxxxx'
+    ##获得磁盘使用率
+    s4 = t[3].split(' ')
+    #for i4 in range(len(s4)):
+    #    print i4, s4[i4]
+    ior = float(s4[24])
+    iow = float(s4[32])
+
+    print 'cpu:%lf mem:%d (rxkB:%lf txkB:%lf) (ior:%lf iow:%lf)' % (i0, i2, rx, tx, ior, iow)
+
+#select事件集合，收集workerServer发送的负载内容，解析后发送给masterServer
 while 1:
     #print 'waiting for next event'
     #得到select返回的3个事件集合，读写异常
@@ -37,17 +111,23 @@ while 1:
     for s in readable:
         #说明有连接加入
         if s is server:
+            #获得连接和客户端ip
             connection, client_address = s.accept()
+            #加入WorkServer集合中，定时向WorkServer发送信号
+            workServer_fd.append(connection)
+            #connection.send('send')
             print " connection from ", client_address
+            #设置非阻塞
             connection.setblocking(False)
             inputs.append(connection)
-            message_queues[connection] = Queue.Queue()
         else:
             date = s.recv(1024)
             result.append(date)
+            #print type(date) 
+            parse(date)
             if date:
-                print " received", date, "from", s.getpeername()
-                message_queues[s].put(date)
+                print " received date from ", s.getpeername()
+                #print date
                 if s not in outputs:
                     outputs.append(s)
             else:
@@ -57,17 +137,6 @@ while 1:
                 inputs.remove(s)
                 s.close()
                 del message_queues[s]
-    #处理写事件集合
-   # for s in writable:
-   #     try:
-   #         next_msg = message_queues[s].get_nowait()
-   #     except Queue.Empty:
-   #         print " ", s.getpeername(), "queue empty"
-   #         outputs.remove(s)
-   #     else:
-   #         print "sending", next_msg, "to ", s.getpeername()
-   #         s.send(next_msg)
-
     #处理异常
     for s in exceptional:
         print "exception condition on", s.getpeername()
@@ -75,8 +144,5 @@ while 1:
         if s in outputs:
             outputs.remove(s)
         s.close()
-        del message_queues[s]
 
-print 'haha'
-
-
+tid.join()
